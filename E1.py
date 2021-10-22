6236d7f7a2b6979894730bccbee1c4383a78083d
mahdi zarepour <mahdizarepour15@gmail.com>
2021-10-22 11:57:53 +0330

    Descriptor __get__ added.

---
+++ E1.py
@@ -0,0 +1,43 @@
+"""
+
+    __dict__
+
+"""
+
+
+
+# -------- __dict__
+
+# class Person:
+#     def __init__(self, name, car):
+#         self.name = name
+#         self.car = car
+
+
+
+# print(Person.__dict__) # {'__module__': '__main__', '__init__': <function Person.__init__ at 0x7f97cc4c31f0>, '__dict__': <attribute '__dict__' of 'Person' objects>, '__weakref__': <attribute '__weakref__' of 'Person' objects>, '__doc__': None}
+
+
+# p = Person('Mahdi', 'genessis')
+# print(p.__dict__) # {'name': 'Mahdi', 'car': 'genessis'}
+# print(p.__dict__['name']) # Mahdi
+
+# --------
+
+
+
+# -------- Descriptor --get--
+
+class One:
+    def __get__(self, instance, owner):
+        return instance
+
+
+class Person:
+    name = One()
+
+
+
+p = Person()
+
+print(Person.name)
